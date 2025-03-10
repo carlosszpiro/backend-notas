f1fd07f907b14dd3c15e24652469decdfddcb664
Carlos Szpiro <carlos.szpiro@outlook.com>
2025-03-10 00:52:23 -0300

    files: edited cors policy

--- config/settings.py
+++ config/settings.py
@@ -85,13 +85,13 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://backend-notas.up.railway.app/"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://127.0.0.1:5173",
+#     "https://backend-notas.up.railway.app/"
+# ]
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
